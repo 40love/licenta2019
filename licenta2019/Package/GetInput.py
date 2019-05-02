ac135f78fe98c01f67909c7b3fe04fbc30afd75d
Robert <sirb.robert@yahoo.com>
2019-05-02 17:23:20 +0300

    vezi cariile prezise

--- licenta2019/Package/GetInput.py
+++ licenta2019/Package/GetInput.py
@@ -8,13 +8,13 @@ import numpy
 import os
 cuvantCheie ="copy"
 directorImagini="..\..\Radiografii_DB"
-dimensiune = (200,200)
+dimensiune = (600,600)
 
 def tranformaImagine(imagine):
     imagine=Image.open(imagine).convert('L')
     #image = color.rgb2gray(imagine)
     image_resized = imagine.resize(dimensiune,Image.ANTIALIAS)
-    #image_resized.save("dinti4.png")
+    image_resized.save("dinti4.png")
     pixels = list(image_resized.getdata())
     width, height = image_resized.size
     pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
@@ -32,7 +32,7 @@ def getPixeliOutput(fisier):
     image = cv2.imread(fisier)
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     lower_red = numpy.array([0,100,100])
-    upper_red = numpy.array([10,255,255])
+    upper_red = numpy.array([179, 255, 255])
     mask = cv2.inRange(hsv, lower_red, upper_red)
     cv2.imshow("HSV", hsv)
     cv2.waitKey(0)
@@ -44,6 +44,8 @@ def getPixeliOutput(fisier):
     # find contours in the thresholded image
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
+    # Create a black image
+    img = numpy.zeros((600,600,3), numpy.uint8)
     for c in cnts:
         # compute the center of the contour
         M = cv2.moments(c)
@@ -54,7 +56,12 @@ def getPixeliOutput(fisier):
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])   
             output.append([cX,cY])
+            cv2.circle(img,(cX,cY), 3, (0,0,255), -1)
+    
+    print(fisier)  
     print(output)
+    cv2.imshow("Mask", img)
+    cv2.waitKey(0)
     return output
 
 def prelucreazaImagineOutput(fisier):
@@ -91,12 +98,25 @@ def prelucreazaImagineMain(fisier):
 
 dictionarImagini={}  #dictionar[NumeImagine] = [intrarePiexeli,iesirePixelCarii]
 
+def resizePoza(fisier):
+    numeComplet = os.path.join(directorImagini,fisier)
+    imagine=Image.open(numeComplet)#.convert('L')
+    image_resized = imagine.resize(dimensiune,Image.ANTIALIAS)
+    if fisier.split('.')[0].endswith("Copy"):
+        numeOutput = fisier.split(" - Copy")[0] +"aux" +" - Copy"+ fisier.split(" - Copy")[1]
+    else:
+        numeOutput = fisier.split(".")[0] + "aux"+"."+fisier.split(".")[1]
+    numeComplet = os.path.join(directorImagini,numeOutput)
+    image_resized.save(numeComplet)
+    return numeOutput
+
 def mainInput():
     from os import listdir
     from os.path import isfile, join
     onlyfiles = [f for f in listdir(directorImagini) if isfile(join(directorImagini, f))]
     for fisier in onlyfiles:
-        if fisier.endswith("jpg") or fisier.endswith("png"):
+        if (fisier.endswith("jpg") or fisier.endswith("png") ) and "aux" not in fisier:
+            fisier = resizePoza(fisier)
             prelucreazaImagineMain(fisier)
     intrare = []
     iesire = []
--- licenta2019/Package/MainScript.py
+++ licenta2019/Package/MainScript.py
@@ -5,10 +5,19 @@ Created on Apr 24, 2019
 '''
 from Package import GetInput, Retea
 import numpy
+limita = 0.90
 def analizeazaPoza(poza):
+    import cv2
     pixeli = GetInput.prelucreazaImagineInput(poza)
+    image = cv2.imread("dinti4.png")
     model = Retea.getModel()
     pred = model.predict(numpy.array([pixeli]))
+    for it in range(len(pred[0])):
+        for jt in range(len(pred[0][it])):
+            if pred[0][it][jt]>limita:
+                cv2.circle(image,(it,jt), 3, (0,0,255), -1)
+    cv2.imshow("IataCarii", image)
+    cv2.waitKey(0)
     print(pred.shape)
     print(pred)
     
